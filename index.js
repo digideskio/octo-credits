698c9c4df2a11dcf69413692e244302c851b7f66
subsidel <joelt.howard@gmail.com>
2015-06-06 20:28:18 +0100

    needless double naming of functions was needless

--- index.js
+++ index.js
@@ -27,63 +27,60 @@ function realNamedCredits(people, accessToken, callback) {
     });
 }
 
-function table(credits) {
-
-    var htmlTable = [
-        '<table>',
-        '<thead><td>Commits</td><td>Name</td><td>Photo</td></thead>',
-
-        function(){
-
-            return credits.map(function (person) {
-
-                return [
-                    '<tr><td>',
-                    person.commits,
-                    '</td><td>',
-                    '<a href=',person.html_url,'>',
-                    person.name,
-                    '</a>',
-                    '</td><td>',
-                    '<img src=',person.avatar,' width="100" height="100" />',
-                    '</td></tr>'
-                ].join('');
-
-            }).join('');
-        }(),
-        '</table>'
-    ].join('');
-
-    return htmlTable;
-}
-
-function retreiveCredits(callback) {
-    var params = {
-        headers: {'user-agent': 'node.js'},
-        url: [
-            'https://api.github.com/repos/',
-            options.repo,
-            '/stats/contributors?access_token=',
-            options.accessToken
-        ].join('')
-    };
-
-    request(params, function (err, res, body) {
-        if (err) console.log(err);
-
-        realNamedCredits(JSON.parse(body), params.accessToken, function (err, credits) {
-            if (err) console.log(err);
-
-            callback(_.sortBy(credits, function(credit) {
-                return -credit.commits;
-            }));
-        });
-    });
-}
-
 module.exports = function(options) {
     return {
-        retreiveCredits: retreiveCredits,
-        table: table
+        retreiveCredits: function (callback) {
+            var params = {
+                headers: {'user-agent': 'node.js'},
+                url: [
+                    'https://api.github.com/repos/',
+                    options.repo,
+                    '/stats/contributors?access_token=',
+                    options.accessToken
+                ].join('')
+            };
+
+            request(params, function (err, res, body) {
+                if (err) console.log(err);
+
+                realNamedCredits(JSON.parse(body), params.accessToken, function (err, credits) {
+                    if (err) console.log(err);
+
+                    callback(_.sortBy(credits, function(credit) {
+                        return -credit.commits;
+                    }));
+                });
+            });
+        },
+
+        table: function (credits) {
+
+            var htmlTable = [
+                '<table>',
+                '<thead><td>Commits</td><td>Name</td><td>Photo</td></thead>',
+
+                function(){
+
+                    return credits.map(function (person) {
+
+                        return [
+                            '<tr><td>',
+                            person.commits,
+                            '</td><td>',
+                            '<a href=',person.html_url,'>',
+                            person.name,
+                            '</a>',
+                            '</td><td>',
+                            '<img src=',person.avatar,' width="100" height="100" />',
+                            '</td></tr>'
+                        ].join('');
+
+                    }).join('');
+                }(),
+                '</table>'
+            ].join('');
+
+            return htmlTable;
+        }
     };
 };
